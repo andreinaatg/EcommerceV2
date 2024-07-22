e722ec1774d68e8e0af29ba43bea61dea64937f5
mtorrealbagarcia@gmail.com <mtorrealbagarcia@gmail.com>
2024-07-22 19:39:55 -0400

    updated file tree

--- frontend/my-app/src/pages/home.js
+++
@@ -1,9 +0,0 @@
-function Home () {
-    return (
-        <div>
-            <p>loremdjvndrnvkdvnkernkwnkwnsknvkjndkndfkvnffbfbfgbfbfg</p>
-        </div>
-    );
-}
-
-export default Home ;
--- frontend/my-app/src/pages/noPage.js
+++
@@ -1,9 +0,0 @@
-function NoPage () {
-    return (
-     <div>
-        <p>idgihdi</p>
-     </div>
-    );
-}
-
-export default NoPage ;
